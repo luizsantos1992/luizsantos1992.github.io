a77416e7d7886596d9470a5e1e63774ce0232098
Luiz Santos <luizsantos1992@gmail.com>
2017-07-26 17:26:14 -0300

    inserindo codigo de barras e digitar codigo de barras

--- JS/Ionic/rastreabilidade/src/pages/rastreabilidade/rastreabilidade.ts
+++ JS/Ionic/rastreabilidade/src/pages/rastreabilidade/rastreabilidade.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+
 
 /**
  * Generated class for the Rastreabilidade page.
@@ -12,46 +14,39 @@ import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 @Component({
   selector: 'page-rastreabilidade',
   templateUrl: 'rastreabilidade.html',
-  providers: [QRScanner]
+  providers: [BarcodeScanner]
 })
 export class RastreabilidadePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private qrScanner: QRScanner) {
+  code : string = '';
+  msgErrCode : string = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private barcodeScanner: BarcodeScanner) {
     
   }
 
   ionViewDidLoad() {
-    this.qrScanner.prepare()
-    .then((status: QRScannerStatus) => {
-      alert(status.authorized);
-      if (status.authorized) {
-        // camera permission was granted
-
-
-        // start scanning
-        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-          console.log('Scanned something', text);
-
-          this.qrScanner.hide(); // hide camera preview
-          scanSub.unsubscribe(); // stop scanning
-        });
-
-        // show camera preview
-        this.qrScanner.show();
-
-        // wait for user to scan something, then the observable callback will be called
-
-      } else if (status.denied) {
-        // camera permission was permanently denied
-        // you must use QRScanner.openSettings() method to guide the user to the settings page
-        // then they can grant the permission from there
-      } else {
-        // permission was denied, but not permanently. You can ask for permission again at a later time.
-      }
-    })
-    .catch((e: any) => console.log('Error is', e));
-    
     console.log('ionViewDidLoad Rastreabilidade');
+    console.log(this.msgErrCode.length);
+  }
+
+  onBarcodeScanner():void {
+    
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log(barcodeData);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getCode():void {
+    this.msgErrCode = '';
+
+    if(this.code.length == 0){
+      this.msgErrCode = 'Código não localizado';
+    }
+
+    this.code = '';
   }
 
   
